7ea7f30c308a6975867d8ff094ef48bf35f74d2b
Ravi7448 <146948118+Ravi7448@users.noreply.github.com>
2024-01-30 17:13:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,25 @@
-provider "aws" {
-  region     = "us-east-1"
-  
-  
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.33.0"
+    }
+  }
 }
 
-#Create a new EC2 launch configuration
-resource "aws_instance" "ec2_public" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = ["${aws_security_group.ssh-security-group.id}"]
-  subnet_id                   = aws_subnet.public-subnet-1.id
-  associate_public_ip_address = true
-  #user_data                   = "${data.template_file.provision.rendered}"
-  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags = {
-    "Name" = "EC2-PUBLIC"
-  }
-  
- 
+provider "aws" {
+access_key = "AKIAUXG7N5W3PN7SNYHG"
+secret_key = "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"
+region = "us-east-1"
 }
-#Create a new EC2 launch configuration
-resource "aws_instance" "ec2_private" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = ["${aws_security_group.webserver-security-group.id}"]
-  subnet_id                   = aws_subnet.private-subnet-1.id
-  associate_public_ip_address = false
-  #user_data                   = "${data.template_file.provision.rendered}"
-  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
-  lifecycle {
-    create_before_destroy = true
+resource "aws_instance" "my-instance" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.micro"
+  key_name      = "Terra"
+   tags = {
+    Name  = "Terraform"
   }
-  tags = {
-    "Name" = "EC2-PRIVATE"
-  }
- 
 }
-################################
  
 
 
